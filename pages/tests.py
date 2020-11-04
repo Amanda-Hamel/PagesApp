b57f31408fe15f3326f6c19f11fcb69e4f8220ef
amanda-hamel <amanda.hamel@snhu.edu>
2020-11-04 12:01:04 -0500

    Initial commit

---
+++ pages/tests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+__author__ = "Amanda Hamel"
+
+from django.test import SimpleTestCase
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+def test_about_page_status_code(self):
+    response = self.client.get('/about/')
+    self.assertEqual(response.status_code, 200)
---
+++ pages/views.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+__author__ = "Amanda Hamel"
+
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
