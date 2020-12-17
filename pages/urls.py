1cb265ee95075c492817b92660354994ea7ac281
amanda-hamel <amanda.hamel@snhu.edu>
2020-12-17 17:16:18 -0500

    Final Commit

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,9 @@
 __author__ = "Amanda Hamel"
 
 from django.urls import path
+
 from .views import HomePageView, AboutPageView
+
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
     path('', HomePageView.as_view(), name='home'),
